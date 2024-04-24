73eff148b86ee986ccbc064587043bdd568feb4a
muniterra <shutt.oficial@gmail.com>
2024-04-24 15:23:38 -0300

    Programa criado e finalizado

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
 
 public class ContaTerminal {
 
 	public static void main(String[] args) {
 		
-		System.out.println("MDS!!!!!!!!!!!!");
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.printf("Por favor, digite o número da Conta: %n");
+		int numConta = scan.nextInt();
+		
+		System.out.printf("Por favor, digite o número da Agência: %n");
+		String numAgencia = scan.next();
+		
+		System.out.printf("Por favor, digite o nome do cliente: %n");
+		String nomeCliente = scan.next();
+		
+		System.out.printf("Por favor, digite o saldo da conta: %n");
+		double saldo = scan.nextDouble();
 
+		System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, "
+				+ "sua agência é %s, conta %d e seu saldo %.2f já está disponível"
+				+ " para saque", nomeCliente, numAgencia, numConta, saldo);
 	}
 
 }
